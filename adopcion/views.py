8a61ca74e22014b542b501b5e7a09b17ae4a6c11
Walter Pariona <wltrst97@gmail.com>
2017-10-18 15:48:08 -0500

    CRUD de Adopción (parte 2)

--- adopcion/views.py
+++ adopcion/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from adopcion.models import Persona, Solicitud
 from adopcion.forms import PersonaForm, SolicitudForm
 from django.core.urlresolvers import reverse_lazy
@@ -40,3 +40,45 @@ class SolicitudCreate(CreateView):
             return HttpResponseRedirect(self.get_success_url())
         else:
             return self.render_to_response(self.get_context_data(form=form, form2=form2))
+
+class SolicitudUpdate(UpdateView):
+    model = Solicitud
+    second_model = Persona
+    template_name = 'adopcion/solicitud_form.html'
+    form_class = SolicitudForm
+    second_form_class = PersonaForm
+    success_url = reverse_lazy('adopcion:solicitud_listar')
+
+    def get_context_data(self, **kwargs):
+        context = super(SolicitudUpdate, self).get_context_data(**kwargs)
+        pk = self.kwargs.get('pk', 0)  #obteniendo los keywords
+        solicitud = self.model.objects.get(id=pk)  #obtenemos el objeto solicitud
+        persona = self.second_model.objects.get(id=solicitud.persona_id)  #obtenemos el objeto persona
+
+        if 'form' not in context:
+            context['form'] = self.form_class()
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class(instance=persona)
+        context['id'] = pk
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object
+        id_solicitud = kwargs['pk']  #obtenemos el id
+        solicitud = self.model.objects.get(id=id_solicitud)
+        persona = self.second_model.objects.get(id=solicitud.persona_id)
+        form = self.form_class(request.POST, instance=solicitud)  #instance es para que no cree objeto y lo actualice
+        form2 = self.second_form_class(request.POST, instance=persona)
+        if form.is_valid() and form2.is_valid():
+            form.save()
+            form2.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return HttpResponseRedirect(self.get_success_url())
+
+class SolicitudDelete(DeleteView):
+    model = Solicitud
+    template_name = 'adopcion/solicitud_delete.html'
+    success_url = reverse_lazy('adopcion:solicitud_listar')
+
+
